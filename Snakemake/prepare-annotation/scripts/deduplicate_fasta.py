de1705c01480d586f6de1eef51a3f3474ecce759
Charlotte Capitanchik <charlotte.capitanchik@crick.ac.uk>
2022-01-18 17:22:39 +0000

    make py3 compliant

--- Snakemake/prepare-annotation/scripts/deduplicate_fasta.py
+++ Snakemake/prepare-annotation/scripts/deduplicate_fasta.py
@@ -30,7 +30,7 @@ for key, value in seq_dict.items():
     rev_seq_dict.setdefault(value, set()).add(key)
 
 total_non_redundant = len(rev_seq_dict)
-for key in rev_seq_dict.iterkeys():
+for key in rev_seq_dict.keys():
     rev_seq_dict[key] = ";".join(rev_seq_dict[key])
 
 report_name = fasta.replace(".fa","-deduplicated-report.txt")
@@ -41,5 +41,5 @@ report_file.close()
 
 new_ff_name = fasta.replace(".fa", "-deduplicated.fa")
 new_fasta_file = open(new_ff_name, "w")
-for key,value in rev_seq_dict.iteritems():
+for key,value in rev_seq_dict.items():
     new_fasta_file.write(">" + value + "\n" + key + "\n")
